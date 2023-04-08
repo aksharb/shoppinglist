4d5265314f5e33c2b70850cf40ecd140ff9be172
Akshar Bisht <bisht.akshar@gmail.com>
2023-04-08 21:57:56 +0530

    connected the app to th Firebase databse

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-//databse reference url:
+//connecting app to the database:
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"; // initializeApp is a Firebase function
+
+//database reference url:
 const appSettings = {
   databaseURL:
     "https://playground-21f8d-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
+
+const app = initializeApp(appSettings);
+console.log(app);
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
