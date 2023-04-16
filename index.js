8ac48b3129c9084a1cf0d8f013713bf2b2fc8bf8
Akshar Bisht <bisht.akshar@gmail.com>
2023-04-16 08:50:24 +0530

    connected the app to Firebase

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 //connecting app to the database:
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"; // initializeApp is a Firebase function
-
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 //database reference url:
 const appSettings = {
   databaseURL:
-    "https://playground-21f8d-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    "https://realtime-database-c9006-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
 
 const app = initializeApp(appSettings);
-console.log(app);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
 addButtonEl.addEventListener("click", function () {
   let inputValue = inputFieldEl.value;
+  push(shoppingListInDB, inputValue);
   console.log(inputValue);
 });
